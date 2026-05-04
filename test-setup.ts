409d2c712159ee36c05a92fb0c3dd6aac353d1f7
Oscar Lahaie <oscarlahaie83@gmail.com>
2026-05-04 23:11:29 +0200

    chore: prepare 0.1.0-rc.6

--- test-setup.ts
+++ test-setup.ts
@@ -92,6 +92,20 @@ const installDomGlobals = () => {
     disconnect() {}
   }
 
+  for (const [key, value] of [
+    ['Error', Error],
+    ['RangeError', RangeError],
+    ['ReferenceError', ReferenceError],
+    ['SyntaxError', SyntaxError],
+    ['TypeError', TypeError],
+  ] as const) {
+    Object.defineProperty(windowInstance, key, {
+      configurable: true,
+      writable: true,
+      value,
+    });
+  }
+
   assignGlobal('window', windowInstance);
   assignGlobal('document', windowInstance.document);
   assignGlobal('navigator', windowInstance.navigator);
@@ -108,6 +122,14 @@ const installDomGlobals = () => {
   assignGlobal('HTMLCanvasElement', windowInstance.HTMLCanvasElement);
   assignGlobal('SVGElement', windowInstance.SVGElement);
   assignGlobal('Node', windowInstance.Node);
+  assignGlobal('EventTarget', windowInstance.EventTarget);
+  assignGlobal('Event', windowInstance.Event);
+  assignGlobal('CustomEvent', windowInstance.CustomEvent);
+  assignGlobal('FocusEvent', windowInstance.FocusEvent);
+  assignGlobal('InputEvent', windowInstance.InputEvent);
+  assignGlobal('MouseEvent', windowInstance.MouseEvent);
+  assignGlobal('KeyboardEvent', windowInstance.KeyboardEvent);
+  assignGlobal('PointerEvent', windowInstance.PointerEvent);
   assignGlobal('MutationObserver', windowInstance.MutationObserver);
   assignGlobal('Blob', windowInstance.Blob);
   assignGlobal('File', windowInstance.File);
