7343383aa5b5ea72a2251cf77d5f1a04f2b83d79
Oscar Lahaie <oscarlahaie83@gmail.com>
2026-08-13 19:07:18 +0200

    feat(implement): filter tasks by project

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -318,7 +318,7 @@ export function Header({
               </span>
             </div>
 
-            {mode !== 'Chat' ? (
+            {mode === 'Architect' ? (
               <>
                 <div className="ml-2 hidden h-5 w-px shrink-0 bg-border sm:block" />
                 <button
